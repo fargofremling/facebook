86ea624ba041734b415f631805e374bb938728c0
fargofremling <alicia.fremling@gmail.com>
2016-03-06 22:16:07 -0800

    initial commit

---
+++ Facebook/ImageTransition.swift
@@ -0,0 +1,13 @@
+//
+//  ImageTransition.swift
+//  Facebook
+//
+//  Created by Fremling, Alicia (Contractor) on 3/6/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ImageTransition: BaseTransition {
+
+}
